ceaab4a8a11f59c4d1db2769279070d4c8605e66
martin <romero7682@gmail.com>
2021-04-15 08:52:28 -0300

    primer commit

---
+++ index.ts
@@ -0,0 +1,26 @@
+class Album{
+    title:string
+    songs:string[]
+    constructor(title, songs:string[]){
+        this.title=title;
+        this.songs=songs;
+    }
+}
+
+class Banda{
+    members: string[]
+    albums:Album[]
+    constructor(members: string[], albums:Album[]){
+        this.members=members;
+        this.albums=albums;
+
+    }
+}
+
+function main(){
+    const unAlbum = new Album("AlbumDeMarce", []);
+    const unaBanda = new Banda(["marce"], [unAlbum, unAlbum, unAlbum]);
+    console.log(unaBanda.members);
+    console.log(unaBanda.albums);
+}
+main()
